f0f4f6d2d04e5684c4525bcf0b39b9608ba79831
Meerim93 <mamakeeva.m93@gmail.com>
2021-11-27 20:40:15 -0500

    fit: Added all requests

--- src/main/java/qa/meerim/controllers/HelloController.java
+++ src/main/java/qa/meerim/controllers/HelloController.java
@@ -1,12 +1,30 @@
 package qa.meerim.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/") // localhost:8080/
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @GetMapping("/meri") // localhost:8080/meri
+    public String meriGet() {
+        return "Hi, my name is Meri and this is a GET request";
+    }
+
+    @PostMapping("/meri") // localhost:8080/meri
+    public String meriPost() {
+        return "Hi, my name is Meri and this is a POST request";
+    }
+    @PutMapping ("/meri") // localhost:8080/meri
+    public String meriPut() {
+        return "Hi, my name is Meri and this is a PUT request";
+    }
+    @DeleteMapping  ("/meri") // localhost:8080/meri
+    public String meriDelete() {
+        return "Hi, my name is Meri and this is a DELETE request";
+    }
+
 }
