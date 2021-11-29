e60396e01382abb0f7a81e85de6ffe938db694ff
Meerim93 <mamakeeva.m93@gmail.com>
2021-11-28 22:10:44 -0500

    formatting the code

--- src/main/java/qa/meerim/controllers/HelloController.java
+++ src/main/java/qa/meerim/controllers/HelloController.java
@@ -4,25 +4,27 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/") // localhost:8080/
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @GetMapping("/meri") // localhost:8080/meri
+    @GetMapping("/meri")
     public String meriGet() {
         return "Hi, my name is Meri and this is a GET request";
     }
 
-    @PostMapping("/meri") // localhost:8080/meri
+    @PostMapping("/meri")
     public String meriPost() {
         return "Hi, my name is Meri and this is a POST request";
     }
-    @PutMapping ("/meri") // localhost:8080/meri
+
+    @PutMapping("/meri")
     public String meriPut() {
         return "Hi, my name is Meri and this is a PUT request";
     }
-    @DeleteMapping  ("/meri") // localhost:8080/meri
+
+    @DeleteMapping("/meri")
     public String meriDelete() {
         return "Hi, my name is Meri and this is a DELETE request";
     }
