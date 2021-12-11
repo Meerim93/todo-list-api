2390fddf5a9eea4320539d6cde45e4fb5924f2ff
mdemianchuk <nickdemianchuk@gmail.com>
2021-12-11 11:16:38 -0600

    Implement update endpoints

---
+++ src/main/java/qa/meerim/controllers/TaskController.java
@@ -0,0 +1,61 @@
+package qa.meerim.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import qa.meerim.models.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TaskController {
+
+    private List<Task> todoList;
+
+    public TaskController() {
+        todoList = new ArrayList<>();
+    }
+
+    @GetMapping("/")
+    public List<Task> getAllTasks() {
+        return todoList;
+    }
+
+
+    @PostMapping("/")
+    public Task addTask(@RequestBody String text) {
+        Task task = new Task(text);
+        todoList.add(task);
+        return task;
+    }
+
+    @GetMapping("/{taskIndex}")
+    public Task getTaskByIndex(@PathVariable int taskIndex) {
+        if (taskIndex >= todoList.size()) {
+            throw new IllegalArgumentException("The task at index " + taskIndex + " doesn't exist.");
+        } else {
+            return todoList.get(taskIndex);
+        }
+    }
+
+    @PostMapping("/{taskIndex}")
+    public Task updateTaskByIndex(@PathVariable int taskIndex) {
+        if (taskIndex >= todoList.size()) {
+            throw new IllegalArgumentException("The task at index " + taskIndex + " doesn't exist.");
+        } else {
+            Task taskToUpdate = todoList.get(taskIndex);
+            taskToUpdate.setIsCompleted(true);
+            return taskToUpdate;
+        }
+    }
+
+    @PutMapping("/{taskIndex}")
+    public Task updateTaskByIndex(@PathVariable int taskIndex, @RequestBody String text) {
+        if (taskIndex >= todoList.size()) {
+            throw new IllegalArgumentException("The task at index " + taskIndex + " doesn't exist.");
+        } else {
+            Task taskToUpdate = todoList.get(taskIndex);
+            taskToUpdate.setText(text);
+            return taskToUpdate;
+        }
+    }
+}
--- src/main/java/qa/meerim/models/Task.java
+++ src/main/java/qa/meerim/models/Task.java
@@ -4,12 +4,35 @@ public class Task {
     // add fields for tasks
     private String text;
     // a field indicating if a task is completed
-
+    private boolean isCompleted;
     // creation datetime
 
     // completion datetime
 
     // create constructor that accepts 1 parameter - String text
+    public Task(String text) {
+        setText(text);
+        setIsCompleted(false);
+    }
 
     // create getters and setters
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        if (text.length() == 0) {
+            throw new IllegalArgumentException("The task text cannot be empty");
+        } else {
+            this.text = text;
+        }
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
 }
